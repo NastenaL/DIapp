1805bac1badca2941b2acbc74d0cf2b507e68051
Настя Настя <analiakh17@gmail.com>
2020-09-23 11:50:14 +0300

    Add extension

---
+++ DIapp/Services/ServiceProviderExtensions.cs
@@ -0,0 +1,12 @@
+﻿namespace DIapp.Services
+{
+    using Microsoft.Extensions.DependencyInjection;
+
+    public static class ServiceProviderExtensions
+    {
+        public static void AddTimeService(this IServiceCollection services)
+        {
+            services.AddTransient<TimeService>();
+        }
+    }
+}
